bffc783788eb7dd641dfe18386fc8206ba2edbab
CocVu <cocvu99@users.noreply.github.com>
2019-11-19 23:05:49 +0700

    build my distribution

--- chapter2/nester/setup.py
+++ chapter2/nester/setup.py
@@ -1,11 +1,9 @@
-Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:21:23) [MSC v.1916 32 bit (Intel)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> from distutils.core import setup
+from distutils.core import setup
 setup(
 	name 		= 'nester',
-	version 	='1.0.0',
+	version 	= '1.0.0',
 	py_modules 	=['nester'],
-	author 		='hfpython',
+	author 		= 'hfpython',
 	author_email 	= 'hfpython@headfirstlabs.com',
 	url 		= 'https://headfirstlabs.com',
 	description 	= 'A simple printer of nested lists',
